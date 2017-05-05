2c384c04a40d1fe7b1867bd59e3f2e5ca3a734d2
chenxiu <cx@pp-panda.com>
2017-05-05 15:43:03 +0800

    Initial commit

---
+++ app/src/main/java/com/pppanda/fragment/HomeFragment.java
@@ -0,0 +1,44 @@
+package com.pppanda.fragment;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.pppanda.R;
+
+/**
+ * Created by Administrator on 2017/5/4.
+ */
+
+//public class HomeFragment extends Fragment {
+//    @Override
+//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+//                             Bundle savedInstanceState) {
+//        return inflater.inflate(R.layout.fragment_home, container, false);
+//    }
+//}
+public class HomeFragment extends Fragment {
+    Context context;
+    public void setContext(Context context) {
+        this.context = context;
+    }
+
+    public HomeFragment(){
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+//        startActivity(new Intent(getActivity(), LoginActivity.class));跳转页面
+
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false);
+
+    }
+
+
+}
