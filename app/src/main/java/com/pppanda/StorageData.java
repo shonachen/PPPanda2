60578cfdfafcac5eaf0d996d03f53b13f62ace7e
chenxiu <cx@pp-panda.com>
2017-05-09 09:32:42 +0800

    22

--- app/src/main/java/com/pppanda/StorageData.java
+++ app/src/main/java/com/pppanda/StorageData.java
@@ -1,8 +1,11 @@
 package com.pppanda;
 
+import android.app.Activity;
+
 /**
  * Created by Administrator on 2017/5/8.
  */
 
-public class StorageData {
+public class StorageData extends Activity{
+
 }
